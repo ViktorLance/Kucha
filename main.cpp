c5d26ecca986c03801371ca07c99ba3d3ee7f7d2
Viktor Kuzemichev <oll.kuz@yandex.ru>
2017-12-01 16:00:24 +0300

    skelet

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include "stdafx.h" 
+#include <iostream> 
+using namespace std; 
+int main()
+{
+	cout << "Vibirite deistvie: 1-clojenie; 2-vichitanie; 3-ymnojenie; 4-delenie: "; 
+	cin >> count; 
+	switch (count) 
+	{ 
+	case 1: 
+	{ // +
+		//cout << " case 1" << endl; 
+		  break; 
+	case 2: 
+	{ // -
+		//cout << "case 2" << endl; 
+		  break; 
+	} 
+case 3: 
+{ // *
+	 //cout << ""<< endl; 
+	break; 
+} 
+case 4: 
+
+{ // /
+	cout << a << " / " << b << " = " << a / b << endl; 
+	break; 
+}
+	//cout << "" << endl;
+	system("pause");
+	return 0;
+}
