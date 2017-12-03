2048e31d8b22a0f1ca0a1d8ce9d2b5f4bde19336
NikitaKunilsky <kunilsky@yandex.ru>
2017-12-03 17:57:04 +0300

    developed main program with 2 functions Summ() and Vich()

--- main.cpp
+++ main.cpp
@@ -1,32 +1,42 @@
 #include "stdafx.h" 
 #include <iostream> 
+#include "lul.h"
 using namespace std; 
 int main()
 {
+	int count;
+	double a=0;
+	double b=0;
+	
+	cout << "Vvedite dva chisla: ";
+	cin >> a;
+	cin >> b;
 	cout << "Vibirite deistvie: 1-clojenie; 2-vichitanie; 3-ymnojenie; 4-delenie: "; 
 	cin >> count; 
-	switch (count) 
-	{ 
-	case 1: 
+	switch (count)
+	{
+	case 1:
 	{ // +
-		//cout << " case 1" << endl; 
-		  break; 
-	case 2: 
+		cout << Summ(a, b) << endl;
+		break;
+	}
+	case 2:
 	{ // -
-		//cout << "case 2" << endl; 
-		  break; 
-	} 
-case 3: 
-{ // *
+		cout << Vich(a, b) << endl;
+		break;
+	}
+	case 3:
+	{ // *
 	 //cout << ""<< endl; 
-	break; 
-} 
-case 4: 
+		break;
+	}
+	case 4:
 
-{ // /
-	cout << a << " / " << b << " = " << a / b << endl; 
-	break; 
-}
+	{ // /
+	//cout << a << " / " << b << " = " << a / b << endl; 
+		break;
+	}
+	}
 	//cout << "" << endl;
 	system("pause");
 	return 0;
